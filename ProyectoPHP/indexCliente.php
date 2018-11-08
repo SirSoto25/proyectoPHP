8222de71913c3479b37c310b88265fceb5c022f7
SirSoto25 <alejandrosoto77@gmail.com>
2018-11-08 18:41:52 +0100

    reservas

--- ProyectoPHP/indexCliente.php
+++ ProyectoPHP/indexCliente.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['pulsado'])) {
         while ($fila = $resul->fetch(PDO::FETCH_ASSOC)) {
             $nombre = $fila['nombre'];
             $apellidos = $fila['apellidos'];
+            $idcliente = $fila['idclientes'];
         }
         echo '<nav>
         <div class="nav-wrapper  deep-purple darken-4 white-text">
@@ -27,6 +28,70 @@ if (isset($_SESSION['pulsado'])) {
           </ul>
         </div>
       </nav>';
+
+        $consultar = "SELECT * FROM habitaciones WHERE estado LIKE '%Libre%'";
+        $resul = $conex->prepare($consultar);
+        $resul->execute();
+
+        if ($resul->rowCount() > 0) {
+            echo '
+            <div class="row">
+            <div class="col s10 offset-s1">
+            <table class="highlight">
+                <thead>
+                    <tr>
+                        <td>Número de Habitación</td>
+                        <td>Hotel</td>
+                        <td>Reservar</td>
+                    </tr>
+                </thead>
+                <tbody>';                
+            while ($fila = $resul->fetch(PDO::FETCH_ASSOC)) {
+                echo '
+                    <tr>
+                        <td>'.$fila["idhabitaciones"].'</td>
+                        <td>'.$fila["hoteles_nombre"].'</td>
+                        <td>
+                        <form action="" method="post">
+                            <input type="password" value="'.$fila["idhabitaciones"].'" name="habitacion" hidden="hidden">
+                            <button class="btn waves-effect waves-light deep-purple darken-3" type="submit" name="reservar">Reservar</button>
+                        </form>
+                        </td>
+                    </tr>';
+            }
+            echo '
+                </tbody>
+            </table>
+            </div>
+            </div>';
+
+            if(isset($_POST['reservar'])){
+                $habitacion = $_POST['habitacion'];
+                $consultaMax = "SELECT MAX(idreserva) FROM reservas";
+                $resul = $conex->prepare($consultaMax);
+                $resul->execute();
+
+                while($fila = $resul->fetch(PDO::FETCH_ASSOC)){
+                    $idReserva = $fila["MAX(idreserva)"] + 1;
+                }
+                $consultar = "INSERT INTO reservas VALUES(".$idReserva.",".$idcliente.",".$habitacion.")";
+                $resul = $conex->prepare($consultar);
+                $resul->execute();
+
+                $update = "UPDATE habitaciones SET estado='Ocupada' WHERE idhabitaciones=".$habitacion;
+                $resul = $conex->prepare($update);
+                $resul->execute();
+
+                header("Location:redirecciones.php");
+            }
+        }else{
+            echo '
+            <div class="row">
+                <div class="col s12 purple-text">
+                    <h1>Todos nuestros hoteles están llenos. Lo sentimos</h1>
+                </div>
+            </div>';
+        }
     } else {
         header("Location:index.php");
     }
